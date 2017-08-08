717f267b59d5ca0b4a4f6cc3350cc31df78c4a60
Jeremy Bowyer <jeremybowyer@gmail.com>
2017-08-08 12:37:25 -0400

    Bound views in Java using ButterKnife. Created initial member variables for total points and hit points.

--- app/src/main/java/com/jeremybowyer/mathtap/MainActivity.java
+++ app/src/main/java/com/jeremybowyer/mathtap/MainActivity.java
@@ -2,12 +2,38 @@ package com.jeremybowyer.mathtap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+
+    private int mTotalPoints = 0;
+    private int mHitPoints = 3;
+
+    @BindView(R.id.equationView) TextView mEquationView;
+
+    @BindView(R.id.answer1) Button mAnswer1;
+    @BindView(R.id.answer2) Button mAnswer2;
+    @BindView(R.id.answer3) Button mAnswer3;
+    @BindView(R.id.answer4) Button mAnswer4;
+    @BindView(R.id.answer5) Button mAnswer5;
+    @BindView(R.id.answer6) Button mAnswer6;
+    @BindView(R.id.answer7) Button mAnswer7;
+    @BindView(R.id.answer8) Button mAnswer8;
+    @BindView(R.id.answer9) Button mAnswer9;
+
+    @BindView(R.id.bonusPointsView) TextView mBonusPoints;
+
+//    @BindView(R.id.heart1) ImageView mHeart1;
+//    @BindView(R.id.heart2) ImageView mHeart2;
+//    @BindView(R.id.heart3) ImageView mHeart3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
