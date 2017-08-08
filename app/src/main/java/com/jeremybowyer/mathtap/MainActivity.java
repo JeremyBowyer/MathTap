60667ab0ebefdcc01fa72c1d5e892042123863ad
jeremy <jeremybowyer@gmail.com>
2017-08-08 00:23:45 -0400

    Added beginnings of a layout. Still missing heart ImageViews and a TextView to hold total points, as opposed to just points to be won this round.

--- app/src/main/java/com/jeremybowyer/mathtap/MainActivity.java
+++ app/src/main/java/com/jeremybowyer/mathtap/MainActivity.java
@@ -3,11 +3,17 @@ package com.jeremybowyer.mathtap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
 }
